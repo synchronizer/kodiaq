8ec7e133e07f6d6f35c115a04a8218c45b344118
Synchronizer <synchronizer@yandex.ru>
2025-04-25 17:23:37 +0300

    commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 Array.from(document.querySelectorAll('.header')).forEach(header => {
     document.addEventListener('scroll', () => {
-        if (header.getBoundingClientRect().y <= -24) {
+        if (header.getBoundingClientRect().y <= -20) {
             header.classList.add('header_sticked')
         } else {
             header.classList.remove('header_sticked')
